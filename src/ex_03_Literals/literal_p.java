76ae1cc9c3f9f1e20cd78fdb47089d2f95a2e0ad
Sainadh Rao <sainadhrao30@gmail.com>
2025-12-28 13:31:02 +0530

    commiting literals

---
+++ src/ex_03_Literals/literal_p.java
@@ -0,0 +1,34 @@
+package ex_03_Literals;
+
+public class literal_p {
+    public static void main(String[] args) {
+        int age = 65;
+        // data_type - int
+        // variable_name(identifier ) age
+        // = (operator)
+        // literal - 65 (int)
+
+        final float pi = 3.14f;
+        //  Literal - float
+
+        final int girl_age = 18;
+        //girl_age = 19;
+
+        boolean is_married = true;
+        boolean is_married_amit = false;
+
+        int age1 = 65; // Decimal System - base wil 10
+
+        // Binary Literal
+        int binary_num = 0b1010; // Binary System
+        // https://www.rapidtables.com/convert/number/binary-to-decimal.html?x=1010
+
+
+        // Octal base? -> 8
+        int octal = 0101;
+
+        // Hexadecimal Literals - 16
+        int hex = 0Xface;
+        //base 16 color combination are in hex - #28a745
+    }
+}
---
+++ src/ex_03_Literals/non_primitive_literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class non_primitive_literals {
+    public static void main(String[] args) {
+        String name = "Sainadh"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+
+        char A = '0';
+        System.out.println(A);
+    }
+}
